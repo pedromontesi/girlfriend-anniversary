93e37cf8216e1b3e683c9aca6ed50b6c53b618c0
pedromontesi <joaopedromonteiro090@hotmail.com>
2025-09-04 18:45:24 -0300

    routes changed

--- public/view/script.js
+++ public/view/script.js
@@ -1,5 +1,5 @@
-import SimpleAnime from './plugin/animation/simple-anime.js';
-import initNumberAnimation from './src/module/js/animation-num.js';
+import SimpleAnime from '../animation/simple-anime.js';
+import initNumberAnimation from '../module/js/animation-num.js';
 
 new SimpleAnime();
 initNumberAnimation()
